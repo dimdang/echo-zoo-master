6eef2568bf5888a7b0e0ba5585407fdf6e5365b4
dangdim <dangdim.hrd@gmail.com>
2019-08-15 03:23:18 +0700

    update war

--- src/main/java/org/cool/zoo/ZooApplication.java
+++ src/main/java/org/cool/zoo/ZooApplication.java
@@ -2,15 +2,23 @@ package org.cool.zoo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class ZooApplication {
+public class ZooApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ZooApplication.class, args);
+    }
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ZooApplication.class);
     }
 
     @Bean
